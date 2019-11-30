37ecd7cbe8afd3fcb1b3045303741cd2a403e618
Steve <steve.vernyi@gmail.com>
2019-11-30 01:37:15 -0500

    ContainsDuplicate & SingleNumber

---
+++ TopInterviewQuestions_Easy/Array/ContainsDuplicate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace TopInterviewQuestions_Easy.Array
+{
+    public class ContainsDuplicate
+    {
+        // Probably too cheeky
+        public bool Solution(int[] nums)
+        {
+            return new HashSet<int>(nums).Count != nums.Length;
+        }
+
+        public bool Solution2(int[] nums)
+        {
+            var set = new HashSet<int>(nums.Length);
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                    return true;
+
+                set.Add(num);
+            }
+
+            return false;
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1 }, true )]
+        [InlineData(new[] { 1, 2, 3, 4 }, false )]
+        public void TestSolution(int[] nums, bool expected)
+        {
+            // Given
+
+            // When
+            var answer = Solution2(nums);
+            // Then
+            Assert.Equal(expected, answer);
+        }
+    }
+}
---
+++ TopInterviewQuestions_Easy/Array/SingleNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+
+namespace TopInterviewQuestions_Easy.Array
+{
+    public class SingleNumber
+    {
+        public int Solution(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            System.Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                if (i == nums.Length - 1 || nums[i] != nums[i + 1])
+                    return nums[i];
+            }
+
+            return 0;
+        }
+
+        // Added posthumously - XOR operator. Interesting way to do remove duplicates
+        public int Solution2(int[] nums)
+        {
+            int answer = 0;
+            foreach (var num in nums)
+                answer ^= num;
+
+            return answer;
+        }
+
+        [Theory]
+        [InlineData(new [] { 2, 2, 1 }, 1)]
+        [InlineData(new [] { 4, 1, 2, 1, 2 }, 4)]
+        public void TestSolution(int[] nums, int expected)
+        {
+            // Given
+
+            // When
+            var answer = Solution2(nums);
+            // Then
+            Assert.Equal(expected, answer);
+        }
+    }
+}
