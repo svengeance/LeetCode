716fd93bff4eb0de986f2bbb7380a91bc23d35de
Steve <steve.vernyi@gmail.com>
2019-11-30 22:11:10 -0500

    MoveZeroes

---
+++ TopInterviewQuestions_Easy/Array/MoveZeroes.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace TopInterviewQuestions_Easy.Array
+{
+    public class MoveZeroes
+    {
+        public void Solution(int[] nums)
+        {
+            int toMove = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    toMove++;
+                    continue;
+                }
+
+                nums[i - toMove] = nums[i];
+            }
+
+            for (int i = nums.Length - toMove; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 1, 0, 3, 12 }, new[] { 1, 3, 12, 0, 0 })]
+        [InlineData(new[] { 0, 0, 0 }, new[] { 0, 0, 0 })]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 2, 3, 0, 1 }, new[] { 1, 2, 3, 1, 2, 3, 1, 0, 0 })]
+        public void TestSolution(int[] nums, int[] expected)
+        {
+            // Given
+
+            // When
+            Solution(nums);
+
+            // Then
+            Assert.Equal(expected, nums);
+        }
+    }
+}
