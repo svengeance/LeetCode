21768e9898190e2f2afa3dfad1d9101c2e3dd938
Svengeance <steve.vernyi@gmail.com>
2021-05-08 00:41:30 -0400

    KClosest

---
+++ AmazonQuestions/Easy/KClosest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazonQuestions.Easy
+{
+    // https://leetcode.com/problems/k-closest-points-to-origin/submissions/
+    public class KClosest: TestQuestion<int[][], int, int[][]>
+    {
+        public override (int[][] arg1, int arg2)[] TestCases => new[]
+        {
+            (
+                new[]
+                {
+                    new[] { 1, 3 }, new[] { -2, 2 }
+                },
+                1
+            ),
+            (
+                new[]
+                {
+                    new[] { 3, 3 }, new[] { 5, -1 }, new[] { -2, 4 }
+                },
+                2
+            )
+        };
+
+        public override int[][][] TestAnswers => new[]
+        {
+            new[]
+            {
+                new[] { -2, 2 }
+            },
+            new[]
+            {
+                new[] { 3, 3 }, new[] { -2, 4 }
+            }
+        };
+
+        public override int[][] Solution(int[][] points, int k)
+        {
+            if (k == 0)
+                return Array.Empty<int[]>();
+
+            double DistanceToOrigin(int[] point) => Math.Sqrt((point[0] * point[0]) + (point[1] * point[1]));
+            Array.Sort(points, (left, right) => DistanceToOrigin(left).CompareTo(DistanceToOrigin(right)));
+
+            return points.AsSpan(..k).ToArray();
+        }
+    }
+}
